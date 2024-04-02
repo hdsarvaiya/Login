52cc94c5796063adcc008b60caf851c0cd119cd0
hdsarvaiya <145086716+hdsarvaiya@users.noreply.github.com>
2024-04-02 20:40:07 +0530

    Add files via upload

---
+++ src/db/conn.js
@@ -0,0 +1,9 @@
+const mongoose = require("mongoose");
+
+mongoose
+  .connect('mongodb://127.0.0.1:27017/newdatabase').then(() => {
+    console.log("Connected to MongoDB!");
+  })
+  .catch((error) => {
+    console.error(`Error connecting to MongoDB: ${error}`);
+  });
