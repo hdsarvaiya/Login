36aa3123aec829ef2a484af137d29fecce86b704
hdsarvaiya <145086716+hdsarvaiya@users.noreply.github.com>
2024-07-23 19:00:39 +0530

    Delete src directory

--- src/db/conn.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-mongoose
-  .connect('mongodb+srv://hdsarvaiya142004:bG4prFr61oI9vRB1@cluster0.wykyqsn.mongodb.net/mernstack').then(() => {
-    console.log("Connected to MongoDB!");
-  })
-  .catch((error) => {
-    console.error(`Error connecting to MongoDB: ${error}`);
-  });
